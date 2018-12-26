30c1f1496e07430e83401d618a9896dccf997a28
cmai <807603148@qq.com>
2018-12-26 22:56:05 +0800

    12.26机器上运行的版本代码，后半段还未测试到，只能保证前段训练为正确的。

---
+++ 1226_cmai_run.py
@@ -0,0 +1,103 @@
+import sys
+import h5py
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+fid_train = h5py.File('training.h5', 'r')
+fid_val = h5py.File('validation.h5','r')
+step = 50000
+epoch = 10
+res=[]
+models=[]
+for i in range(18):
+    print(str(i))
+    L_in=keras.layers.Input((32,32,1),name=str(i)+'input')
+
+    X = keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,1),name=str(i)+'con0')(L_in)
+    X = keras.layers.MaxPool2D((2,2),name=str(i)+'poo0')(X)
+    X = keras.layers.Conv2D(32,(3,3),activation='relu',name=str(i)+'con1')(X)
+    X = keras.layers.MaxPool2D((2,2),name=str(i)+'poo1')(X)
+    X = keras.layers.Conv2D(32,(3,3),activation='relu',name=str(i)+'con2')(X)
+    X = keras.layers.Flatten(name=str(i)+'fla')(X)
+    X = keras.layers.Dense(64,activation='relu',name=str(i)+'den0')(X)
+    out1=X
+    X = keras.layers.Dense(17,activation='softmax',name=str(i)+'den1')(X)
+
+    model = keras.models.Model(inputs=L_in,outputs=X)
+    model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
+
+    m0 = keras.models.Model(inputs=L_in, outputs=out1)
+    for now_epoch in range(epoch):
+        for j in range(int(len(fid_train['sen1'])/step)):
+            start_point = j*step
+
+            if j+1 == int(len(fid_train['sen1'])/step):
+                end_point = len(fid_train)
+            else:
+                end_point = (j+1)*step
+            print(str(start_point), str(end_point))
+            # Loading sentinel-1 data patches
+            s1_train = np.array(fid_train['sen1'][start_point:end_point])
+            # Loading sentinel-2 data patches
+            s2_train = np.array(fid_train['sen2'][start_point:end_point])
+            # Loading labels
+            labels_train = np.array(fid_train['label'][start_point:end_point])
+            if i<8:
+                X=s1_train[:,:,:,i].reshape((-1,32,32,1))
+            else:
+                X=s2_train[:,:,:,i-8].reshape((-1,32,32,1))
+            y = labels_train
+
+            model.fit(X,y,epochs=1,batch_size=64)
+            s1_val = np.array(fid_val['sen1'])
+            s2_val = np.array(fid_val['sen2'])
+            labels_val = np.array(fid_val['label'])
+            if i<8:
+                X_val=s1_val[:,:,:,i].reshape((-1,32,32,1))
+            else:
+                X_val=s2_val[:,:,:,i-8].reshape((-1,32,32,1))
+            y_val = labels_val
+            print(model.evaluate(X_val,y_val))
+            models.append(model)
+    model.save('model'+str(i))
+    m0.save('m'+str(i))
+
+models=[]
+for i in range(18):
+    models.append(keras.models.load_model('model'+str(i)))
+
+X=keras.layers.Concatenate()([models[i].output for i in range(18)])
+X=keras.layers.Reshape((18,-1))(X)
+X=keras.layers.Conv1D(12,1)(X)
+X=keras.layers.Flatten()(X)
+X=keras.layers.Dense(17,activation='softmax')(X)
+
+model=keras.models.Model(inputs=[models[i].input for i in range(18)],outputs=X)
+
+
+model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
+for i in range(int(len(fid_train['sen1'])/step)):
+    start_point = j * step
+    if j + 1 == int(len(fid_train['sen1']) / step):
+        end_point = len(fid_train)
+    else:
+        end_point = (j + 1) * step
+    # Loading sentinel-1 data patches
+    s1_train = np.array(fid_train['sen1'][start_point:end_point])
+    # Loading sentinel-2 data patches
+    s2_train = np.array(fid_train['sen2'][start_point:end_point])
+    # Loading labels
+    labels_train = np.array(fid_train['label'][start_point:end_point])
+    model.fit([s1_train[:,:,:,i].reshape((-1,32,32,1)) for i in range(8)]
+              +[s2_train[:,:,:,i].reshape((-1,32,32,1)) for i in range(10)]
+              ,labels_train,epochs=10)
+    s1_val = np.array(fid_val['sen1'])
+    s2_val = np.array(fid_val['sen2'])
+    labels_val = np.array(fid_val['label'])
+    res=model.evaluate([s1_val[:,:,:,i].reshape((-1,32,32,1))for i in range(8)]
+                       +[s2_val[:,:,:,i].reshape((-1,32,32,1)) for i in range(10)]
+                       ,labels_val)
+    print(res)
+exit()
