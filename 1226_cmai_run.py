ed6762f8f8751d81afbecefa1652ce5eb37347af
cmai <807603148@qq.com>
2018-12-28 09:31:31 +0800

    12.27gpu跑的版本。

--- 1226_cmai_run.py
+++ 1226_cmai_run.py
@@ -1,16 +1,21 @@
 import sys
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 import h5py
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tensorflow import keras
-fid_train = h5py.File('training.h5', 'r')
-fid_val = h5py.File('validation.h5','r')
+fid_train = h5py.File('../training.h5', 'r')
+fid_val = h5py.File('../validation.h5','r')
+train_len = len(fid_train['sen1'])
 step = 50000
-epoch = 10
+epoch = 1
+train_epoch = 10
 res=[]
 models=[]
+m0s=[]
 for i in range(18):
     print(str(i))
     L_in=keras.layers.Input((32,32,1),name=str(i)+'input')
@@ -29,29 +34,31 @@ for i in range(18):
     model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
 
     m0 = keras.models.Model(inputs=L_in, outputs=out1)
-    for now_epoch in range(epoch):
-        for j in range(int(len(fid_train['sen1'])/step)):
-            start_point = j*step
+    m0s.append(m0)
+    models.append(model)
+for now_epoch in range(epoch):
+    for j in range(int(train_len/step)):
+        start_point = j*step
 
-            if j+1 == int(len(fid_train['sen1'])/step):
-                end_point = len(fid_train)
-            else:
-                end_point = (j+1)*step
-            print(str(start_point), str(end_point))
-            # Loading sentinel-1 data patches
-            s1_train = np.array(fid_train['sen1'][start_point:end_point])
-            # Loading sentinel-2 data patches
-            s2_train = np.array(fid_train['sen2'][start_point:end_point])
-            # Loading labels
-            labels_train = np.array(fid_train['label'][start_point:end_point])
-            if i<8:
-                X=s1_train[:,:,:,i].reshape((-1,32,32,1))
-            else:
-                X=s2_train[:,:,:,i-8].reshape((-1,32,32,1))
-            y = labels_train
-
-            model.fit(X,y,epochs=1,batch_size=64)
-            s1_val = np.array(fid_val['sen1'])
+        if j+1 == int(train_len/step):
+            end_point = train_len
+        else:
+            end_point = (j+1)*step
+        print(str(start_point), str(end_point))
+        # Loading sentinel-1 data patches
+        s1_train = np.array(fid_train['sen1'][start_point:end_point])
+        # Loading sentinel-2 data patches
+        s2_train = np.array(fid_train['sen2'][start_point:end_point])
+        # Loading labels
+        labels_train = np.array(fid_train['label'][start_point:end_point])
+        if i<8:
+            X=s1_train[:,:,:,i].reshape((-1,32,32,1))
+        else:
+            X=s2_train[:,:,:,i-8].reshape((-1,32,32,1))
+        y = labels_train
+        for model in models:
+            model.fit(X,y,epochs=train_epoch,batch_size=64)
+            s1_val= np.array(fid_val['sen1'])
             s2_val = np.array(fid_val['sen2'])
             labels_val = np.array(fid_val['label'])
             if i<8:
@@ -60,16 +67,20 @@ for i in range(18):
                 X_val=s2_val[:,:,:,i-8].reshape((-1,32,32,1))
             y_val = labels_val
             print(model.evaluate(X_val,y_val))
-            models.append(model)
+i = 0
+for model in models:
     model.save('model'+str(i))
-    m0.save('m'+str(i))
+    i+=1
+    # m0.save('m'+str(i))
 
 models=[]
 for i in range(18):
     models.append(keras.models.load_model('model'+str(i)))
 
 X=keras.layers.Concatenate()([models[i].output for i in range(18)])
-X=keras.layers.Reshape((18,-1))(X)
+print(X)
+X=keras.layers.Reshape((18,17))(X)
+print(X)
 X=keras.layers.Conv1D(12,1)(X)
 X=keras.layers.Flatten()(X)
 X=keras.layers.Dense(17,activation='softmax')(X)
@@ -78,21 +89,22 @@ model=keras.models.Model(inputs=[models[i].input for i in range(18)],outputs=X)
 
 
 model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
-for i in range(int(len(fid_train['sen1'])/step)):
-    start_point = j * step
-    if j + 1 == int(len(fid_train['sen1']) / step):
-        end_point = len(fid_train)
-    else:
-        end_point = (j + 1) * step
-    # Loading sentinel-1 data patches
-    s1_train = np.array(fid_train['sen1'][start_point:end_point])
-    # Loading sentinel-2 data patches
-    s2_train = np.array(fid_train['sen2'][start_point:end_point])
-    # Loading labels
-    labels_train = np.array(fid_train['label'][start_point:end_point])
-    model.fit([s1_train[:,:,:,i].reshape((-1,32,32,1)) for i in range(8)]
-              +[s2_train[:,:,:,i].reshape((-1,32,32,1)) for i in range(10)]
-              ,labels_train,epochs=10)
+for now_epoch in range(epoch):
+    for i in range(int(train_len/step)):
+        start_point = j * step
+        if j + 1 == int(train_len / step):
+            end_point = train_len
+        else:
+            end_point = (j + 1) * step
+        # Loading sentinel-1 data patches
+        s1_train = np.array(fid_train['sen1'][start_point:end_point])
+        # Loading sentinel-2 data patches
+        s2_train = np.array(fid_train['sen2'][start_point:end_point])
+        # Loading labels
+        labels_train = np.array(fid_train['label'][start_point:end_point])
+        model.fit([s1_train[:,:,:,i].reshape((-1,32,32,1)) for i in range(8)]
+                  +[s2_train[:,:,:,i].reshape((-1,32,32,1)) for i in range(10)]
+                  ,labels_train,epochs=train_epoch,batch_size=64)
     s1_val = np.array(fid_val['sen1'])
     s2_val = np.array(fid_val['sen2'])
     labels_val = np.array(fid_val['label'])
@@ -100,4 +112,4 @@ for i in range(int(len(fid_train['sen1'])/step)):
                        +[s2_val[:,:,:,i].reshape((-1,32,32,1)) for i in range(10)]
                        ,labels_val)
     print(res)
-exit()
+model.save('result')
---
+++ 1227_cmai_run.py
@@ -0,0 +1,131 @@
+import sys
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+import h5py
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+import gc
+is_val = True
+res_size=[(64,64,256),(128,128,512),(256,256,1024),(512,512,2048)]
+L=[3,4,6,2]
+
+L_input1=keras.layers.Input((32,32,8))
+L_input2=keras.layers.Input((32,32,10))
+
+X=keras.layers.Concatenate()([L_input1,L_input2])
+
+
+X=keras.layers.Conv2D(64,(3,3),activation='relu',padding='same')(X)
+X=keras.layers.BatchNormalization(axis=-1)(X)
+X=keras.layers.MaxPool2D((2,2))(X)
+
+models=[]
+
+
+for i in range(4):
+
+    print(i)
+
+    for j in range(L[i]):
+
+        print((i,j))
+
+        key=res_size[i]
+        if j==0:
+            X_short = keras.layers.Conv2D(key[2],(1,1),activation='relu',padding='same')(X)
+            X_short = keras.layers.BatchNormalization()(X_short)
+        else:
+            X_short = X
+        X = keras.layers.Conv2D(key[0],(1,1),activation='relu',padding='same')(X)
+        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.Conv2D(key[1], (3, 3), activation='relu',padding='same')(X)
+        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.Conv2D(key[2], (3, 3), activation='relu', padding='same')(X)
+        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.Add()([X,X_short])
+        X = keras.layers.Activation('relu')(X)
+
+
+        out = keras.layers.AveragePooling2D((2, 2))(X)
+        out = keras.layers.Flatten()(out)
+        out = keras.layers.Dense(64,activation='relu')(out)
+        out = keras.layers.Dense(17,activation='softmax')(out)
+        model = keras.models.Model(inputs=[L_input1, L_input2], outputs=out)
+        model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+        models.append(model)
+
+X=keras.layers.AveragePooling2D((2,2))(X)
+X=keras.layers.Flatten()(X)
+L_out=keras.layers.Dense(17,activation='softmax')(X)
+
+model=keras.models.Model(inputs=[L_input1,L_input2],outputs=L_out)
+model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
+
+fid_train = h5py.File('../training.h5', 'r')
+fid_val = h5py.File('../validation.h5','r')
+step=50000
+length = len(fid_train['sen1'])
+res = []
+s1,s2,labels=None,None,None
+if is_val:
+    s1_val = fid_val['sen1']
+    s2_val = fid_val['sen2']
+    labels_val = fid_val['label']
+layers = 0
+for mm in models:
+
+    for i in range(int(length / step)):
+        # Loading sentinel-1 data patches
+        start_point = i*step
+        if i+1 == int(length/step):
+            end_point = length
+        else:
+            end_point = (i+1) * step
+        del s1,s2,labels
+        gc.collect()
+        print ('loading')
+        s1 = np.array(fid_train['sen1'][start_point:end_point])
+        # Loading sentinel-2 data patches
+        s2 = np.array(fid_train['sen2'][start_point:end_point])
+        # Loading labels
+        labels = np.array(fid_train['label'][start_point:end_point])
+
+        mm.fit(x=[s1[:45000],s2[:45000]],y=labels[:45000],epochs=1,batch_size=64)
+        if is_val:
+            temp = mm.evaluate(x=[s1[45000:], s2[45000:]], y=labels[45000:])
+            temp0 = mm.evaluate(x=[s1[:45000], s2[:45000]], y=labels[:45000])
+            print('')
+            print('---------------------------------------')
+            print(temp)
+            print(temp0)
+    if is_val:
+        temp=mm.evaluate(x=[s1_val,s2_val],y=labels_val)
+        print('')
+        print('---------------------------------------')
+        print(temp)
+        res.append( temp )
+    mm.save(str(layers)+'-models')
+acc=[]
+for i in range(10):
+
+    for i in range(int(length / step)):
+        start_point = i*step
+        if i+1 == int(length/step):
+            end_point = length
+        else:
+            end_point = (i+1) * step
+        # Loading sentinel-1 data patches
+        s1 = np.array(fid_train['sen1'][start_point:end_point])
+        # Loading sentinel-2 data patches
+        s2 = np.array(fid_train['sen2'][start_point:end_point])
+        # Loading labels
+        labels = np.array(fid_train['label'][start_point:end_point])
+
+        model.fit(x=[s1,s2],y=labels,epochs=1,batch_size=64)
+    if is_val:
+        acc.append( model.evaluate(x=[s1_val,s2_val],y=labels_val) )
+    model.save(str(i)+'-epoch')
+exit()
